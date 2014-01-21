25ed8113a7d055ecda099e5caf21223f5cd74769
yap cheah shen <yapcheahshen@gmail.com>
2014-01-21 23:12:32 +0800

    markup devolve ok

--- kdoc.js
+++ kdoc.js
@@ -1,11 +1,35 @@
+/*
+  Multiversion text with external durable markups
+*/
+
+var reverseRevision=function(revs,parenttext) {
+	var reversed=[];
+	revs.sort(function(m1,m2){return m1.start-m2.start});
+	var offset=0;
+	revs.map(function(r){
+		m=cloneMarkup(r);
+		if (parenttext) {
+			newtext=parenttext.substr(r.start,r.len);
+		} else {//just a dummy text with same len
+			newtext=Array(m.len+1).join("-");
+		}
+		m.start+=offset;
+		m.len=m.payload.text.length;
+		m.payload.text=newtext;
+		offset+=m.len-newtext.length;
+		reversed.push(m);
+	})
+	return reversed;
+}
 var evolveMarkup=function(markup, rev) {
 	end=markup.start+markup.len;
-	newlen=(rev.payload.length-rev.len);
+	newlen=(rev.payload.text.length-rev.len);
 	revend=rev.start+rev.len;
-	var m=JSON.parse(JSON.stringify(markup)); //return a new copy
+
+	var m=cloneMarkup(markup); //return a new copy
 
 	if (end<=rev.start) return m;
-	else if (revend<=markup.start) {	
+	else if (revend<=markup.start) {
 		m.start+=newlen;
 		return m;
 	} else { //overlap
@@ -32,46 +56,100 @@ var evolveText=function(sourcetext ,revisions) {
 	revisions.sort(function(r1,r2){return r2.start-r1.start});
 	var text=sourcetext;
 	revisions.map(function(r){
-		text=text.substring(0,r.start)+r.payload+text.substring(r.start+r.len);
+		text=text.substring(0,r.start)+r.payload.text+text.substring(r.start+r.len);
 	});
 	return text;
 }
-var createMarkup=function(start,len,isRevision,payload) {
-	return {start:start,len:len,isRevision:isRevision||false,payload:payload};
+var createMarkup=function(textlen,start,len,payload) {
+	if (textlen==-1) textlen=1024*1024*1024; //max string size 1GB
+	//the only function create a new markup instance, be friendly to V8 Hidden Class
+
+	if (len<0) len=textlen;
+	if (start<0) start=0;
+	if (start>textlen) start=textlen;
+	if (start+len>textlen) {
+		len-=start+len-textlen;
+		if (len<0) len=0;
+	}
+
+	return {start:start,len:len,payload:payload};
+}
+var cloneMarkup=function(m) {
+	return createMarkup(-1,m.start,m.len,JSON.parse(JSON.stringify(m.payload)));
 }
 var createDocument = function(opts) {
 	var text="";
 
 	opts=opts||{};
-	if (opts.parent) text=opts.parent.getText();
+	parentId=0;
+	if (opts.parent) {
+		text=opts.parent.getText();
+		parentId=opts.parent.getId();
+	} 
 	
 	var doc={};
 	opts.id=opts.id || 0;
-	var metadata= {id:opts.id, parent: opts.parent, date: new Date() };
-	var revisions= null;
+	var meta= {id:opts.id, parentId:parentId, date: new Date() , revisions:null};
 
 	var markups=[];
+	var revisions=[];
 	var getText=function() {	return text;	}
 	var getId=function() {	return id;	}
+	var getParentId=function() {	return parentId;	}
 
-	var addMarkup=function(start,len,str) {
-		markups.push(createMarkup(start, len, false,str ));
+	var addMarkup=function(start,len,payload) {
+		markups.push(createMarkup(text.length,start, len, payload ));
 	}
-	var addRevision=function(start,len,payload) {
-		markups.push(createMarkup(start, len, true,payload ));
+	var addMarkups=function(newmarkups,clear) {
+		if (clear) markups=[];
+		newmarkups.map(function(m){
+			markups.push(createMarkup(text.length, m.start, m.len, m.payload ));	
+		})
+	}
+	var addRevisions=function(newrevisions,clear) {
+		if (clear) revisions=[];
+		newrevisions.map(function(m){
+			revisions.push(createMarkup(text.length, m.start, m.len, m.payload ));	
+		})
+	}
+
+	var addRevision=function(start,len,str) {
+		revisions.push(createMarkup(text.length,start, len, {text:str} ));
 	}
 	var clearRevisions=function() {
-		markups=markups.filter(function(m){ return (!m.isRevision)});
+		revisions=[];
 	}
-	var evolve=function(rev) {
-		text=evolveText(text, rev);
-		revisions=rev;
-		//evolve markups
-		//markups=markupEvolve(markups,revisions);
+	var evolve=function(revs,M) {
+		text=evolveText(text, revs);
+		meta.revisions=revs;
+		evolvedmarkups=[];
+		M.map(function(m){
+			revs.map(function(rev){
+				m=evolveMarkup(m,rev);
+			});
+			evolvedmarkups.push(m);
+		})
+		markups=evolvedmarkups;
+	}
+
+	var devolveMarkups=function(markups) {
+		reverse=reverseRevision(meta.revisions);
+		devolvedmarkups=[];
+		reverse.sort(function(a,b){return b.start-a.start});
+		markups.map(function(m){
+			reverse.map(function(rev){
+				m=evolveMarkup(m,rev);
+			});
+			devolvedmarkups.push(m);
+		})
+		return devolvedmarkups;
 	}
 
 	var getRevisions=function() {
-		return markups.filter(function(m){ return (m.isRevision)});
+		return revisions;
+	}
+	var getMarkups=function() {
+		return markups;
 	}
 
 	//interfaces
@@ -79,9 +157,14 @@ var createDocument = function(opts) {
 
 	doc.getText=getText;
 	doc.getId=getId;
+	doc.getParentId=getParentId;
 	doc.addMarkup=addMarkup;
+	doc.addMarkups=addMarkups;
 	doc.addRevision=addRevision;
+	doc.addRevisions=addRevisions;
 	doc.getRevisions=getRevisions;
+	doc.getMarkups=getMarkups;
+	doc.devolveMarkups=devolveMarkups;
 	doc.clearRevisions=clearRevisions;
 
 	return doc;
@@ -104,7 +187,7 @@ var Database = function() {
 			doc=createDocumentFromRoot({id:id,text:input})
 		} else {
 			id=doccount++;
-			parent=input||root;
+			parent=input||0;
 			doc=createDocument({id:id,parent:parent});
 		}
 
@@ -119,8 +202,20 @@ var Database = function() {
 
 	var evolveDocument=function(d,opts) {
 		var nextgen=newDocument(d);
-		nextgen.__evolve__( d.getRevisions() );
+		nextgen.__evolve__( d.getRevisions() , d.getMarkups() );
 		return nextgen;
+	}
+
+	var migrateTo=function(markups,revs) {
+
+	}
+	var migrate=function(from,to) {
+		var M=null;
+		if (typeof to=='undefined') {
+			M=from.devolveMarkups(from.getMarkups());
+		}
+
+		return M;
 	}
 
 	return {
@@ -128,7 +223,9 @@ var Database = function() {
 		getDocument: getDocument,
 		getDocumentCount:getDocumentCount,
 		evolveDocument:evolveDocument,
-		evolveMarkup:evolveMarkup
+		evolveMarkup:evolveMarkup,
+		reverseRevision:reverseRevision,
+		migrate:migrate
 	}
 }
 
--- test/document-test.js
+++ test/document-test.js
@@ -1,7 +1,5 @@
 var K=require('../kdoc');
-
 console.log('ksana document test suite');
-
 
 QUnit.test('new document',function(){
 	var db=new K.Database();
@@ -11,34 +9,33 @@ QUnit.test('new document',function(){
 	equal(db.getDocumentCount(),2);
 });
 
-
 QUnit.test('evolve markup (delete)',function(){
 	var db=new K.Database();
-	deletemiddle={start:3,len:3,payload:""};
-
-	left={start:0,len:3,payload:"***"};
+	deletemiddle={start:3,len:3,payload:{text:""}};
+	payload={nothing:true}
+	left={start:0,len:3,payload:payload};
 	m=db.evolveMarkup( left , deletemiddle) //
-	equal(JSON.stringify(m), JSON.stringify(left));
+	equal(m.start,0);equal(m.len,3);
 
-	right={start:6,len:3,payload:"***"};
+	right={start:6,len:3,payload:payload};
 	m=db.evolveMarkup( right , deletemiddle) //
-	equal(JSON.stringify(m), JSON.stringify({start:3,len:3,payload:"***"}));	
+	equal(m.start,3);equal(m.len,3);
 
-	middle={start:4,len:1,payload:"***"};
+	middle={start:4,len:1,payload:payload};
 	m=db.evolveMarkup( middle , deletemiddle) 
-	equal(JSON.stringify(m), JSON.stringify({start:3,len:0,payload:"***"}));
+	equal(m.start,3);equal(m.len,0);
 
-  leftpartial={start:2,len:3,payload:"***"};
+  leftpartial={start:2,len:3,payload:payload};
 	m=db.evolveMarkup( leftpartial , deletemiddle);
-	equal(JSON.stringify(m), JSON.stringify({start:2,len:1,payload:"***"}));
+	equal(m.start,2);equal(m.len,1);
 
-	partial={start:2,len:5,payload:"***"};
+	partial={start:2,len:5,payload:payload};
 	m=db.evolveMarkup( partial , deletemiddle) 
-	equal(JSON.stringify(m), JSON.stringify({start:2,len:2,payload:"***"}));
+	equal(m.start,2);equal(m.len,2);
 
-  rightpartial={start:4,len:3,payload:"***"};
+  rightpartial={start:4,len:3,payload:payload};
 	m=db.evolveMarkup( rightpartial , deletemiddle);
-	equal(JSON.stringify(m), JSON.stringify({start:3,len:1,payload:"***"}));
+	equal(m.start,3);equal(m.len,1);
 });
 	//insert text
 		//  markup    x    xx      xx      xyz      xyyyz        xyz  
@@ -47,37 +44,39 @@ QUnit.test('evolve markup (delete)',function(){
 		//  insert   +++ x +++xx  +++ xx   x+++yz   x+++yyyz    +++ xyz
 QUnit.test('evolve markup (insert)',function(){
 	var db=new K.Database();
-	insertmiddle={start:3,len:0,payload:"+++"};
+	payload={nothing:true}
+	insertmiddle={start:3,len:0,payload:{text:"+++"}};
 
-	left={start:0,len:3,payload:"***"};
+	left={start:0,len:3,payload:payload};
 	m=db.evolveMarkup( left , insertmiddle) // no change
-	equal(JSON.stringify(m), JSON.stringify(left));
+	equal(m.start,0);equal(m.len,3);
 
-	right={start:6,len:3,payload:"***"};
+	right={start:6,len:3,payload:payload};
 	m=db.evolveMarkup( right , insertmiddle) //
-	equal(JSON.stringify(m), JSON.stringify({start:9,len:3,payload:"***"}));	
+	equal(m.start,9);equal(m.len,3);
 
-	middle={start:4,len:1,payload:"***"};
+	middle={start:4,len:1,payload:payload};
 	m=db.evolveMarkup( middle , insertmiddle) 
-	equal(JSON.stringify(m), JSON.stringify({start:7,len:1,payload:"***"}));
+	equal(m.start,7);equal(m.len,1);
 
-  leftpartial={start:2,len:3,payload:"***"};
+  leftpartial={start:2,len:3,payload:payload};
 	m=db.evolveMarkup( leftpartial , insertmiddle);
-	equal(JSON.stringify(m), JSON.stringify({start:2,len:6,payload:"***"}));
+	equal(m.start,2);equal(m.len,6);
 
-	partial={start:2,len:5,payload:"***"};
+	partial={start:2,len:5,payload:payload};
 	m=db.evolveMarkup( partial , insertmiddle) 
-	equal(JSON.stringify(m), JSON.stringify({start:2,len:8,payload:"***"}));
+	equal(m.start,2);equal(m.len,8);
 
-  rightpartial={start:4,len:3,payload:"***"};
+  rightpartial={start:4,len:3,payload:payload};
 	m=db.evolveMarkup( rightpartial , insertmiddle);
-	equal(JSON.stringify(m), JSON.stringify({start:7,len:3,payload:"***"}));
+	equal(m.start,7);equal(m.len,3);
 
 });
+
+var origin="道可道非常道名可名非常名";
+
 QUnit.test('evolve document',function(){
 	var db=new K.Database();
-
-	var origin="道可道非常道名可名非常名";
 	var daodejin=db.newDocument(origin);
 	equal(daodejin.getId(),1);
 
@@ -87,9 +86,15 @@ QUnit.test('evolve document',function(){
 	daodejin.addRevision(6,0,'也');
 	daodejin.addRevision(10,1,'恆');
 	daodejin.addRevision(12,0,'也');
+	daodejin.addMarkup(2,1,{name:"動詞"});
+	daodejin.addMarkup(8,1,{name:"動詞"});
 
 	mawang=db.evolveDocument(daodejin)
 	equal(mawang.getText(),"道可道非恆道也名可名非恆名也");
+	markups=mawang.getMarkups();
+
+	equal(markups[0].start, 2);
+	equal(markups[1].start, 9); 
 
 	daodejin.clearRevisions(); //prepare for new evolution
 	daodejin.addRevision(3,0,"，");
@@ -99,13 +104,65 @@ QUnit.test('evolve document',function(){
 	punc=db.evolveDocument(daodejin);
 	equal(punc.getText(),"道可道，非常道；名可名，非常名。");
 
-	equal(db.getDocumentCount(),4);//root,d,daodejin,mawang,punc
+	equal(db.getDocumentCount(),4);//root,daodejin,mawang,punc
+
 	//ng=db.coevolve(mawang,punc);
 	//equal(ng.getText(),"道可道，非恆道也；名可名，非恆名也。");
-
-
-	daodejin.addMarkup(2,1,{name:"動詞"});
-	daodejin.addMarkup(8,1,{name:"動詞"});
 });
 
+QUnit.test('validate markup position',function() {
+	var db=new K.Database();
+	var daodejin=db.newDocument(origin);
+	daodejin.addMarkup(0,-1,{empty:true});
+	daodejin.addMarkup(-10,-1,{empty:true});
+	daodejin.addMarkup(-10,5,{empty:true});
+	daodejin.addMarkup(13,2,{empty:true});
+	daodejin.addMarkup(10,10,{empty:true});
 
+	markups=daodejin.getMarkups();
+	equal(markups[0].start,0,'markup 1 start');
+	equal(markups[0].len,origin.length,'markup 1 length');
+	equal(markups[1].start,0,'markup 2 start');
+	equal(markups[1].len,origin.length,'markup 2 length');
+	equal(markups[2].start,0,'markup 3 start');
+	equal(markups[2].len,5,'markup 3 length');
+	equal(markups[3].start,12,'markup 4 start');
+	equal(markups[3].len,0,'markup 4 length');
+	equal(markups[4].start,10,'markup 5 start');
+	equal(markups[4].len,2,'markup 5 length');
+
+});
+QUnit.test('devolve markups',function(){
+	var db=new K.Database();
+	var daodejin=db.newDocument(origin+"。");
+	daodejin.addRevision(6,0,'也');
+	daodejin.addRevision(12,0,'也');
+	mawang=db.evolveDocument(daodejin);
+	//道可道非常道也名可名非常名也。
+	reverse=db.reverseRevision(daodejin.getRevisions(),daodejin.getText());
+	mawang.addRevisions(reverse);
+	//道可道非常道名可名非常名。
+	daodejin2=db.evolveDocument(mawang);
+	equal(daodejin2.getText(),origin+"。","rollback with reverse revision"); 
+
+	mawang.addMarkup(13,1,{name:"虛字"});
+	mawang.addMarkup(9,1,{name:"動詞"});
+	mawang.addMarkup(5,3,{name:"道也名"}); 
+
+	M=db.migrate(mawang);
+	
+	equal(M[0].start,12,'markup 1 start');
+	equal(M[0].len,0,'markup 1 len'); //vanish
+
+	equal(M[1].start,8,'markup 2 start');
+	equal(M[1].len,1,'markup 2 len'); //survive
+	equal(daodejin2.getText().substr(M[1].start,M[1].len),"名");
+	
+	equal(M[2].start,5,'markup 3 start');
+	equal(M[2].len,2,'markup 3 len'); //survive but content changed
+	equal(daodejin2.getText().substr(M[2].start,M[2].len),"道名");
+	
+});
+QUnit.test('coevolve document',function(){
+	equal(true,true)
+});
